2d74a2d2933c81a95e8074b9766caa774e543ed2
lusika <lusika777>
2024-04-29 21:03:47 +0400

    testing both pages, delete extra, work on toolbar on main page, add pages buttons, add mobile logic with toolbar

--- src/components/LoginPage.vue
+++ src/components/LoginPage.vue
@@ -1,34 +1,30 @@
 
 <template>
-  <div class="login-page">
-    <h1 class="text-h2 text-center"> Spotify Shuffler {{ msg }}</h1>
-    <v-container>
-      <v-col>
-        <v-card border="opacity-10" class="mx-auto" max-width="1000" min-height="200" rounded="xl">
-          <v-card class="mx-auto" max-width="800" min-height="50">
-            <p class="font-weight-light text-end">
-              Do you know that the algorythm for shuffleling that Spotify using make you listen to same 10 songs over and
-              over?
-              <br>
-              Want to shuffle your playlist true randomly?
-            </p>
-            <p>
-              <v-btn href="https://github.com/lusikawurtz" target="_blank">
-                Login with Spotify
-              </v-btn>
-            </p>
+  <v-app>
+    <div class="login-page">
+      <v-toolbar app dark color="black" height="120">
+        <v-toolbar-title class="text-h2 text-center"> Spotify Shuffler </v-toolbar-title>
+      </v-toolbar>
+      <v-container>
+        <v-col>
+          <v-card border="opacity-10" class="mx-auto" max-width="1000" min-height="200" rounded="xl">
+            <v-card class="mx-auto" max-width="800" min-height="50">
+              <p class="font-weight-light text-end">
+                Do you know that the algorythm for shuffleling that Spotify using make you listen to same 10 songs over
+                and
+                over?
+                <br>
+                Want to shuffle your playlist true randomly?
+              </p>
+              <p>
+                <v-btn href="https://github.com/lusikawurtz" target="_blank">
+                  Login with Spotify
+                </v-btn>
+              </p>
+            </v-card>
           </v-card>
-        </v-card>
-      </v-col>
-    </v-container>
-  </div>
+        </v-col>
+      </v-container>
+    </div>
+  </v-app>
 </template>
-
-<script>
-export default {
-  name: 'MainPage',
-  props: {
-    msg: String,
-  }
-}
-</script>
